4fcc5fb07ba0114fb454bee2e0334aa7fa7a59ba
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-05 18:21:57 +0530

    [Shivani] UC4 - Line Comparision by oops concept and use Equals and CompareTo Method

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose a option\n 1. Length of line\n 2. Two line comparision using Equals method\n 3. Two line comparision using CompareTo method");
+            Console.WriteLine("Choose a option\n 1. Length of line\n 2. Two line comparision using Equals method\n 3. Two line comparision using CompareTo method\n 4. Two line comparision by oops concept");
             Console.WriteLine("Enter the number");
             int option=Convert.ToInt32(Console.ReadLine());
 
@@ -18,6 +18,15 @@
                     break;
                 case 3:
                     LineComparisionCompareToMethod.CheckLengthCompareToMethod();
+                    break;
+                case 4:
+                    LineComparisionOopsConcept obj = new LineComparisionOopsConcept();
+                    obj.SetLength1(2, 4, 6, 8);
+                    obj.GetLength1();
+                    obj.SetLength2(2, 4, 6, 8);
+                    obj.GetLength2();
+                    obj.display1();
+                    obj.display2();
                     break;
                 default:
                     Console.WriteLine("Choose a rigth number");
