86374e278508ab4249432bf6867a26100acf5f96
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-05 17:09:56 +0530

    [Shivani] Line comparision problem

---
+++ LineComparision/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LineComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a option\n ");
+            Console.WriteLine("Enter the number");
+            int option=Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                
+                break;
+                default:
+                    Console.WriteLine("Choose a rigth number");
+                    break;
+            }
+        }
+    }
+}
