618520508eb32367ca7564c0a8f70e9c9a3e874f
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-05 17:19:13 +0530

    [Shivani] UC1 - Length of line

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -4,14 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose a option\n ");
+            Console.WriteLine("Choose a option\n 1. Length of line");
             Console.WriteLine("Enter the number");
             int option=Convert.ToInt32(Console.ReadLine());
 
             switch (option)
             {
                 case 1:
-                
+                    LengthOfLine.CalculateLengthOfLine();
                 break;
                 default:
                     Console.WriteLine("Choose a rigth number");
